2bd1de21b2e62b6a0c2a279798a205bfda9e3b0a
Théo Dancoisne <theo.dancoisne@outlook.fr>
2023-08-18 22:43:04 +0200

    handling december-january exception

--- e2e/schedule.spec.ts
+++ e2e/schedule.spec.ts
@@ -21,13 +21,19 @@ import { test, expect } from '@playwright/test';
 test("connect to wigor", async ({ page }) => {
     test.slow();
     const url = "https://ws-edt-cd.wigorservices.net/WebPsDyn.aspx?action=posEDTLMS&amp;serverID=C&amp;Tel=theo.dancoisne&amp;date=03/28/2023&amp;hashURL=6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2";
-    const dayAtPos = {
-        103: "monday",
-        122.4: "tuesday",
-        141.8: "wednesday",
-        161.2: "thursday",
-        180.6: "friday",
-        // + 0.12
+    const translateMonth = {
+        janvier: 1,
+        février: 2,
+        mars: 3,
+        avril: 4,
+        mai: 5,
+        juin: 6,
+        juillet: 7,
+        aout: 8,
+        septembre: 9,
+        octobre: 10,
+        novembre: 11,
+        décembre: 12,
     };
     var dateAtPos = {
         103: null,
@@ -35,40 +41,54 @@ test("connect to wigor", async ({ page }) => {
         141.8: null,
         161.2: null,
         180.6: null,
-    }
-    const translateMonth = {
-        "janvier": "01",
-        "février": "02",
-        "mars": "03",
-        "avril": "04",
-        "mai": "05",
-        "juin": "06",
-        "juillet": "07",
-        "aout": "08",
-        "septembre": "09",
-        "octobre": "10",
-        "novembre": "11",
-        "décembre": "12",
     };
-    const users = require("../credentials_n_tokens/wigor_schedule/.users.json");
-    page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D03%252F28%252F2023%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
-    await page.getByLabel("username").fill(users.sys.unm);
-    await page.getByLabel("password").fill(users.sys.pswd);
+    const users = require("../credentials_n_tokens/wigor_schedule/users.json");
+
+    // page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D3%252F28%252F2023%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
+    // page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D12%252F30%252F2024%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
+    // page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D01%252F02%252F2025%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
+    page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D01%252F7%252F2025%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
+    await page.getByLabel("username").fill(users["3sys"].unm);
+    await page.getByLabel("password").fill(users["3sys"].pswd);
     await page.getByRole("button", { name: 'Login' }).click();
 
     await page.waitForLoadState("networkidle");
     // date format in the url: m/d/y
-    const dateInUrl = page.url().match(/date=(.*)&/)[1].split("/").map((item) => parseInt(item));
     // month is 0 indexed, 0 = january  |  getDay() return number between 0 and 6, 0 = sunday (not monday!)
-    const dayInUrl = new Date(dateInUrl[2], dateInUrl[0] - 1, dateInUrl[1]).getDay();
+    const dateInUrl = page.url().match(/date=(.*)&/)[1].split("/").map((item) => parseInt(item));
+    const dayInUrl = new Date(dateInUrl[2], dateInUrl[0] - 1, dateInUrl[1]);
+
+    // EXCEPTION: the date in the url does not enable us to have the correct year between the 24 December and the 07 January
+    if (dayInUrl.getMonth() == 11 && new Date("12/24/" + dayInUrl.getFullYear()) < dayInUrl && dayInUrl < new Date("1/7/" + (dayInUrl.getFullYear() + 1))) {
+        // var janDecException = 31 - dayInUrl.getDate() + dayInUrl.getDay();
+        var janDecException = true;
+    } else if (dayInUrl.getMonth() == 0 && new Date("12/24/" + (dayInUrl.getFullYear() - 1)) < dayInUrl && dayInUrl < new Date("1/7/" + dayInUrl.getFullYear())) {
+        // var janDecException = dayInUrl.getDay() - dayInUrl.getDate();
+        var janDecException = true;
+    // } else var janDecException = 0;
+    } else var janDecException = false;
+    // EXCEPTION
+    
     const days = page.locator(".Jour");
     let dateAtPosKeys = Object.keys(dateAtPos);
     // 5 because they load 5 day before and after the current week
     for (let i = 5; i < await days.count() - 5; i++) {
         let day = (await days.nth(i).locator(".TCJour").textContent()).toLowerCase().split(" ").slice(1);
-        dateAtPos[dateAtPosKeys[i - 5]] = {"d": parseInt(day[0]), "m": parseInt(translateMonth[day[1]])};
+        dateAtPos[dateAtPosKeys[i - 5]] = {
+            d: parseInt(day[0]),
+            m: translateMonth[day[1]],
+            y: (() => {
+                if (janDecException) {
+                    if (translateMonth[day[1]] == 1 && 1 == dayInUrl.getMonth() + 1) return dayInUrl.getFullYear();
+                    if (translateMonth[day[1]] == 12 && 12 == dayInUrl.getMonth() + 1) return dayInUrl.getFullYear();
+                    else {
+                        if (translateMonth[day[1]] == 1) return dayInUrl.getFullYear() + 1;
+                        else return dayInUrl.getFullYear() - 1;
+                    }
+                } else return dayInUrl.getFullYear();
+            })()
+        };
     }
-    console.log(dateAtPos);
     const events = page.locator(".Case");
     for (let i = 0; i < await events.count() - 1; i++) {
         let content = await events.nth(i).getByRole("table");
