c8006470e28da4d00c47a27b228e3897ad8c3e11
Théo Dancoisne <theo.dancoisne@outlook.fr>
2023-08-21 19:35:01 +0200

    optimized part of the script (thank you ChatGPT for convincing me), testing server implementation

--- e2e/schedule.spec.ts
+++ e2e/schedule.spec.ts
@@ -44,10 +44,10 @@ test("connect to wigor", async ({ page }) => {
     };
     const users = require("../credentials_n_tokens/wigor_schedule/users.json");
 
-    // page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D3%252F28%252F2023%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
+    page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D3%252F28%252F2023%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
     // page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D12%252F30%252F2024%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
     // page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D01%252F02%252F2025%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
-    page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D01%252F7%252F2025%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
+    // page.goto("https://cas-p.wigorservices.net/cas/login?service=https%3A%2F%2Fws-edt-cd.wigorservices.net%2FWebPsDyn.aspx%3Faction%3DposEDTLMS%26serverID%3DC%26Tel%3Dtheo.dancoisne%26date%3D01%252F7%252F2025%26hashURL%3D6A322522A712EBD110260D1D505E28F595156D9701C0D240D268F8F329899514AFCCC45DAE4C54C0329C0765F10306871431A8FDA76A5C561114CD87028866D2");
     await page.getByLabel("username").fill(users["3sys"].unm);
     await page.getByLabel("password").fill(users["3sys"].pswd);
     await page.getByRole("button", { name: 'Login' }).click();
@@ -59,56 +59,59 @@ test("connect to wigor", async ({ page }) => {
     const dayInUrl = new Date(dateInUrl[2], dateInUrl[0] - 1, dateInUrl[1]);
 
     // EXCEPTION: the date in the url does not enable us to have the correct year between the 24 December and the 07 January
-    if (dayInUrl.getMonth() == 11 && new Date("12/24/" + dayInUrl.getFullYear()) < dayInUrl && dayInUrl < new Date("1/7/" + (dayInUrl.getFullYear() + 1))) {
-        // var janDecException = 31 - dayInUrl.getDate() + dayInUrl.getDay();
-        var janDecException = true;
-    } else if (dayInUrl.getMonth() == 0 && new Date("12/24/" + (dayInUrl.getFullYear() - 1)) < dayInUrl && dayInUrl < new Date("1/7/" + dayInUrl.getFullYear())) {
-        // var janDecException = dayInUrl.getDay() - dayInUrl.getDate();
-        var janDecException = true;
-    // } else var janDecException = 0;
-    } else var janDecException = false;
+    const janDecException = (() => {
+        const firstDayOfWeek = new Date(dayInUrl);
+        firstDayOfWeek.setDate(firstDayOfWeek.getDate() - dayInUrl.getDay() + 1);   // +1 because otherwise it will give you the Sunday of the previous week at 00:00:00 and so the last day will be the Saturday at 00:00:00, no false but not what I want
+        const lastDayOfWeek = new Date(firstDayOfWeek);
+        lastDayOfWeek.setDate(firstDayOfWeek.getDate() + 6);
+
+        return firstDayOfWeek.getFullYear() !== lastDayOfWeek.getFullYear();
+    })();
     // EXCEPTION
     
     const days = page.locator(".Jour");
     let dateAtPosKeys = Object.keys(dateAtPos);
     // 5 because they load 5 day before and after the current week
     for (let i = 5; i < await days.count() - 5; i++) {
-        let day = (await days.nth(i).locator(".TCJour").textContent()).toLowerCase().split(" ").slice(1);
+        const day = (await days.nth(i).locator(".TCJour").textContent()).toLowerCase().split(" ").slice(1);
+        const TranslateMonth = translateMonth[day[1]];
+        const currentMonth = dayInUrl.getMonth() + 1;
         dateAtPos[dateAtPosKeys[i - 5]] = {
             d: parseInt(day[0]),
-            m: translateMonth[day[1]],
+            m: TranslateMonth,
             y: (() => {
                 if (janDecException) {
-                    if (translateMonth[day[1]] == 1 && 1 == dayInUrl.getMonth() + 1) return dayInUrl.getFullYear();
-                    if (translateMonth[day[1]] == 12 && 12 == dayInUrl.getMonth() + 1) return dayInUrl.getFullYear();
-                    else {
-                        if (translateMonth[day[1]] == 1) return dayInUrl.getFullYear() + 1;
-                        else return dayInUrl.getFullYear() - 1;
-                    }
+                    if (TranslateMonth == 1 && 1 == currentMonth || TranslateMonth == 12 && 12 == currentMonth) {
+                        return dayInUrl.getFullYear();
+                    } else return dayInUrl.getFullYear() + (TranslateMonth == 1 ? 1 : -1);
                 } else return dayInUrl.getFullYear();
             })()
         };
     }
     const events = page.locator(".Case");
-    for (let i = 0; i < await events.count() - 1; i++) {
-        let content = await events.nth(i).getByRole("table");
-        
-        let position = events.nth(i).getAttribute("style").then((style) => {
-            // @ts-ignore
-            style = style.replaceAll(" ", "").split(/[:;]/);
-            // @ts-ignore
-            return style[style.indexOf("left") + 1].slice(0, -1);
-        });
-        
-        let title = await content.locator(".TCase").first().textContent();
-        let link = await content.locator(".TCase").locator(".Teams").locator("a").first().getAttribute("href");
-        let teacher = (await content.locator(".TCProf").innerText()).split("\n");
-        let schoolYear = teacher.pop();
-        let start = (await content.locator(".TChdeb").textContent()).split(" - ");
-        let end = start.pop();
-        // text content exemple: "Salle:<>(<>)"
-        // my regex sucks because it leaves an empty string at both ends of the array so I added filter()
-        let room = (await content.locator(".TCSalle").textContent()).split(/Salle:|\(|\)/).filter(item => item);
-        let location = room.pop();
+    if (await events.first().textContent() == "Pas de cours cette semaine") {
+        console.log("nah");
+    } else {
+        for (let i = 0; i < await events.count() - 1; i++) {
+            let content = await events.nth(i).getByRole("table");
+            
+            let position = events.nth(i).getAttribute("style").then((style) => {
+                // @ts-ignore
+                style = style.replaceAll(" ", "").split(/[:;]/);
+                // @ts-ignore
+                return style[style.indexOf("left") + 1].slice(0, -1);
+            });
+            
+            let title = await content.locator(".TCase").first().textContent();
+            let link = await content.locator(".TCase").locator(".Teams").locator("a").first().getAttribute("href");
+            let teacher = (await content.locator(".TCProf").innerText()).split("\n");
+            let schoolYear = teacher.pop();
+            let start = (await content.locator(".TChdeb").textContent()).split(" - ");
+            let end = start.pop();
+            // text content exemple: "Salle:<>(<>)"
+            // my regex sucks because it leaves an empty string at both ends of the array so I added filter()
+            let room = (await content.locator(".TCSalle").textContent()).split(/Salle:|\(|\)/).filter(item => item);
+            let location = room.pop();
+        }
     }
 });
