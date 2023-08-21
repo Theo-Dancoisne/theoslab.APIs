275dd3155e037e555a8ac174906909257e98e0f1
Théo Dancoisne <theo.dancoisne@outlook.fr>
2023-08-21 21:18:02 +0200

    server test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-    console.log("Server listening on port ${port}");
+    console.log('Server listening on port ${port}');
 });
 
 app.get("/", (req, res) => {
